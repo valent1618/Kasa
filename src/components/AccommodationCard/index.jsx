b60c92cf929b79462a390105048a18523215cbaf
valent1618 <valentinlemaire1618@gmail.com>
2022-06-30 20:22:15 +0200

    add component card

---
+++ src/components/AccommodationCard/index.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+function AccommodationCard({accommodation}) {
+  return (
+    <Link to={`accommodation/${accommodation.id}`} className='accommodation-card'>
+      <img src={accommodation.cover} alt={accommodation.title}></img>
+      <div className='card-body'>
+        <h4>{accommodation.title}</h4>
+      </div>
+    </Link>
+  )
+}
+
+export default AccommodationCard;
