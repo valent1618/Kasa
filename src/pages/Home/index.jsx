f7e6bcb6d88bbf931b3452305e8654f1a928dd6d
valent1618 <valentinlemaire1618@gmail.com>
2022-06-30 20:24:06 +0200

    add content to the home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from 'react';
+
+import fetchData from '../../functions/fetchData';
+import AccommodationCard from '../../components/AccommodationCard';
+
+
 function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData(setData)
+  }, [])
+
   return (
     <main id="Home">
-      <h1>Accueil</h1>
+      <section className='intro'>
+        <h1><span>Chez vous, </span><span>partout et ailleurs</span></h1>
+      </section>
+      <section className='accommodations'>
+        {data.map((accommodation) => (
+          <AccommodationCard
+            key={`accommodation-${accommodation.id}`}
+            to={`/accommodation/${accommodation.id}`}
+            accommodation={accommodation}
+          />
+        ))}
+      </section>
     </main>
   );
 }
