1dd9f5dfcd7c6054bdd0fd2e139b4bfb6266178e
valent1618 <valentinlemaire1618@gmail.com>
2022-06-30 15:39:57 +0200

    better architecture

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -3,10 +3,8 @@ import { Link } from 'react-router-dom';
 function Error() {
   return (
     <main id="Error">
-      <div className='container'>
-        <h1>404</h1>
-        <h2>Oups! La page que vous demandez n'existe pas.</h2>
-      </div>
+      <h1>404</h1>
+      <h2>Oups! La page que vous demandez n'existe pas.</h2>
       <Link to="/">Retourner sur la page d'accueil</Link>
     </main>
   );
