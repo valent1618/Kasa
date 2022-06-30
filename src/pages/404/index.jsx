bc04d73dccbeda80bc403e586aeaeef453e1594e
valent1618 <valentinlemaire1618@gmail.com>
2022-06-30 15:03:53 +0200

    add content

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function Error() {
   return (
     <main id="Error">
-      <h1>Erreur 404</h1>
+      <div className='container'>
+        <h1>404</h1>
+        <h2>Oups! La page que vous demandez n'existe pas.</h2>
+      </div>
+      <Link to="/">Retourner sur la page d'accueil</Link>
     </main>
   );
 }
