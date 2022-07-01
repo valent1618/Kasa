ab232eef3c4efaeb023857b95d5020bf1242ba55
valent1618 <valentinlemaire1618@gmail.com>
2022-07-01 17:45:40 +0200

    add component gallery

---
+++ src/components/Gallery/index.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react' ;
+
+import { handleGallery, autoGallery } from '../../functions/gallery.js';
+
+function Gallery({pictures}) {
+  const [activeImg, setActiveImg] = useState(1);
+
+  useEffect(() => {
+    autoGallery(4000);
+  }, [])
+
+  if(pictures.length > 1) {
+    return (
+      <div className='gallery'>
+        <div className='arrow-container left' onClick={() => handleGallery(activeImg, setActiveImg, pictures.length, 'previous')}>
+          <span className='arrow' />
+        </div>
+        <h4>{activeImg}/{pictures.length}</h4>
+        <div className='arrow-container right' onClick={() => handleGallery(activeImg, setActiveImg, pictures.length, 'next')}>
+          <span className='arrow' />
+        </div>
+        {pictures.map((img, index) => (
+          <img key={`picture-${index}`} src={img} alt={`Aperçu ${index + 1} du logement`} className={activeImg === index + 1 ? "activeImg" : ""} />
+        ))}
+      </div>
+    )
+  } else {
+    return (
+      <div className='gallery'>
+        <img src={pictures[0]} alt={`Aperçu du logement`} className="activeImg" />
+      </div>
+    )
+  }
+}
+
+export default Gallery;
