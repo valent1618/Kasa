4998b4dcbcd489a16e7da754783ea57dc20916cc
valent1618 <valentinlemaire1618@gmail.com>
2022-07-05 17:00:02 +0200

    add assets

---
+++ src/assets/collapse-arrow.jsx
@@ -0,0 +1,19 @@
+function CollapseArrow({ rotate }) {
+  return (
+    <svg
+      width='28'
+      height='17'
+      viewBox='0 0 28 17'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      transform={rotate ? 'scale(-1)' : ''}
+    >
+      <path
+        d='M3.15993 0.653343L0.799927 3.02668L13.9999 16.2133L27.1999 3.01335L24.8399 0.653343L13.9999 11.4933L3.15993 0.653343Z'
+        fill='white'
+      />
+    </svg>
+  );
+}
+
+export default CollapseArrow;
