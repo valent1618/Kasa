465b94bfad894d4bcd36ba87756d593230dfbfc8
valent1618 <valentinlemaire1618@gmail.com>
2022-07-05 17:35:51 +0200

    improve autoGallery

--- src/functions/gallery.js
+++ src/functions/gallery.js
@@ -8,28 +8,45 @@ export function handleGallery(activeImg, setActiveImg, numberImg, action) {
   }
 }
 
-let interval = false;
+/* - Auto Gallery - */
+let interval = null;
+
+function animateImg(img) {
+  img.classList.remove('activeImg');
+
+  // add delay to have the transition
+  setTimeout(() => {
+    img.classList.add('activeImg');
+  }, 100);
+}
+
+function runGallery(time, gallery) {
+  const next = gallery.getElementsByClassName('arrow-container')[1];
+
+  interval = setInterval(() => {
+    next.click();
+  }, time);
+}
 
 export function autoGallery(time) {
-  const next = document.getElementsByClassName('arrow-container')[1];
   const gallery = document.getElementsByClassName('gallery')[0];
+  const firstImg = gallery.getElementsByTagName('img')[0];
 
-  if (!interval) {
-    interval = setInterval(() => {
-      next.click();
-    }, time);
+  // Check if autoGallery has already run, if not run
+  if (gallery.getAttribute('data-auto') === null) {
+    gallery.setAttribute('data-auto', 'true');
+
+    // add and remove the class on the first img to see the transition
+    animateImg(firstImg);
+
+    runGallery(time, gallery);
+
+    gallery.addEventListener('mouseover', () => {
+      clearInterval(interval);
+    });
+
+    gallery.addEventListener('mouseout', () => {
+      runGallery(time, gallery);
+    });
   }
-
-  gallery.addEventListener('mouseover', () => {
-    clearInterval(interval);
-    interval = false;
-  });
-
-  gallery.addEventListener('mouseout', () => {
-    if (!interval) {
-      interval = setInterval(() => {
-        next.click();
-      }, time);
-    }
-  });
 }
