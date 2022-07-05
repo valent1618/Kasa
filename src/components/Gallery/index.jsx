39b1734582536aa2d1d0aa1061ce4aab79fc4820
valent1618 <valentinlemaire1618@gmail.com>
2022-07-05 17:38:27 +0200

    call autoGallery conditionally

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,35 +1,56 @@
-import { useState, useEffect } from 'react' ;
+import { useState, useEffect } from 'react';
 
 import { handleGallery, autoGallery } from '../../functions/gallery.js';
 
-function Gallery({pictures}) {
+function Gallery({ pictures }) {
   const [activeImg, setActiveImg] = useState(1);
 
   useEffect(() => {
-    autoGallery(4000);
-  }, [])
+    pictures.length > 1 && autoGallery(4000);
+  }, [pictures]);
 
-  if(pictures.length > 1) {
+  if (pictures.length > 1) {
     return (
       <div className='gallery'>
-        <div className='arrow-container left' onClick={() => handleGallery(activeImg, setActiveImg, pictures.length, 'previous')}>
+        <div
+          className='arrow-container left'
+          onClick={() =>
+            handleGallery(activeImg, setActiveImg, pictures.length, 'previous')
+          }
+        >
           <span className='arrow' />
         </div>
-        <h4>{activeImg}/{pictures.length}</h4>
-        <div className='arrow-container right' onClick={() => handleGallery(activeImg, setActiveImg, pictures.length, 'next')}>
+        <h4>
+          {activeImg}/{pictures.length}
+        </h4>
+        <div
+          className='arrow-container right'
+          onClick={() =>
+            handleGallery(activeImg, setActiveImg, pictures.length, 'next')
+          }
+        >
           <span className='arrow' />
         </div>
         {pictures.map((img, index) => (
-          <img key={`picture-${index}`} src={img} alt={`Aperçu ${index + 1} du logement`} className={activeImg === index + 1 ? "activeImg" : ""} />
+          <img
+            key={`picture-${index}`}
+            src={img}
+            alt={`Aperçu ${index + 1} du logement`}
+            className={activeImg === index + 1 ? 'activeImg' : ''}
+          />
         ))}
       </div>
-    )
+    );
   } else {
     return (
       <div className='gallery'>
-        <img src={pictures[0]} alt={`Aperçu du logement`} className="activeImg" />
+        <img
+          src={pictures[0]}
+          alt={`Aperçu du logement`}
+          className='aloneImg'
+        />
       </div>
-    )
+    );
   }
 }
 
