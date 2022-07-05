e971029f2a2b8f4a68d3920bd3a65632ff9747ee
valent1618 <valentinlemaire1618@gmail.com>
2022-07-05 17:42:51 +0200

    prettier and handle error case

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,32 +1,49 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 import fetchData from '../../functions/fetchData';
-import AccommodationCard from '../../components/AccommodationCard';
 
+import AccommodationCard from '../../components/AccommodationCard';
 
 function Home() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetchData(setData)
-  }, [])
+    fetchData(setData);
+  }, []);
 
-  return (
-    <main id="Home">
-      <section className='intro'>
-        <h1><span>Chez vous, </span><span>partout et ailleurs</span></h1>
-      </section>
-      <section className='accommodations'>
-        {data.map((accommodation) => (
-          <AccommodationCard
-            key={`accommodation-${accommodation.id}`}
-            to={`/accommodation/${accommodation.id}`}
-            accommodation={accommodation}
-          />
-        ))}
-      </section>
-    </main>
-  );
+  if (data === undefined || data.length === 0) {
+    return (
+      <main id='Error'>
+        <h1>404</h1>
+        <h2>
+          Oups! Nous ne trouvons plus les logements. Nous nous chargeons de
+          résoudre ce problème au plus vite.
+        </h2>
+        <Link to='/about'>Voir la page à propos</Link>
+      </main>
+    );
+  } else {
+    return (
+      <main id='Home'>
+        <section className='intro'>
+          <h1>
+            <span>Chez vous, </span>
+            <span>partout et ailleurs</span>
+          </h1>
+        </section>
+        <section className='accommodations'>
+          {data.map((accommodation) => (
+            <AccommodationCard
+              key={`accommodation-${accommodation.id}`}
+              to={`/accommodation/${accommodation.id}`}
+              accommodation={accommodation}
+            />
+          ))}
+        </section>
+      </main>
+    );
+  }
 }
 
 export default Home;
