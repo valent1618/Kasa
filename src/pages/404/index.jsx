5addaf6d6c3acd16f7d4676791245ab5689c5d20
valent1618 <valentinlemaire1618@gmail.com>
2022-07-07 13:50:23 +0200

    handle error for home and accommodation page

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,11 +1,24 @@
 import { Link } from 'react-router-dom';
 
-function Error() {
+function Error({ error = '404' }) {
   return (
     <main id='Error'>
       <h1>404</h1>
-      <h2>Oups! La page que vous demandez n'existe pas.</h2>
-      <Link to='/Kasa'>Retourner sur la page d'accueil</Link>
+      {error === 'home' ? (
+        <h2>
+          Oups! Nous ne trouvons plus les logements. Nous nous chargeons de
+          résoudre ce problème au plus vite.
+        </h2>
+      ) : error === 'accommodation' ? (
+        <h2>Oups! Nous ne trouvons pas le logement que vous cherchez.</h2>
+      ) : (
+        <h2>Oups! La page que vous demandez n'existe pas.</h2>
+      )}
+      {error === 'home' ? (
+        <Link to='/Kasa/about'>Voir la page à propos</Link>
+      ) : (
+        <Link to='/Kasa'>Retourner sur la page d'accueil</Link>
+      )}
     </main>
   );
 }
